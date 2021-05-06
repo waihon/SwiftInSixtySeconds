9136434de6ccc3c4d804eec4652e121d9f034fe9
Waihon Yew <yewwaihon@gmail.com>
2021-05-06 08:40:10 +0800

    Create PrintTerminator.swift

---
+++ Functions/PrintTerminator.swift
@@ -0,0 +1,10 @@
+print("Hello, world!", terminator: " ")
+
+print("Hello, Malaysia!", terminator: " ")
+
+print("Hello, Cheras!")
+
+print("Hello, everyone!")
+
+// Hello, world! Hello, Malaysia! Hello, Cheras!
+// Hello, everyone!
