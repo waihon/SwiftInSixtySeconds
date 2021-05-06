2b1493e27e3fcbc8b74eeefb918cf194544e39d9
Waihon Yew <yewwaihon@gmail.com>
2021-05-06 10:47:05 +0800

    Create ValidatePassword.swift

---
+++ Functions/ValidatePassword.swift
@@ -0,0 +1,23 @@
+enum PasswordError: Error {
+    case obvious
+    case common
+}
+
+func validatePassword(_ password: String) throws -> Bool {
+    let obviousPasswords = ["password", "password1"]
+    let commonPasswords = ["123456", "123456789", "picture1", "12345678", "111111"]
+
+    for obvious in obviousPasswords {
+        if password == obvious {
+            throw PasswordError.obvious
+        }
+    }
+
+    for common in commonPasswords {
+        if password == common {
+            throw PasswordError.common
+        }
+    }
+
+  return true
+}
