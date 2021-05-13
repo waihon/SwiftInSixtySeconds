b60d2d03058ac1d1ea725cb6ae1abc871530a869
Waihon Yew <yewwaihon@gmail.com>
2021-05-14 07:57:39 +0800

    Create Arithmetic.swift

---
+++ Operators/Arithmetic.swift
@@ -0,0 +1,13 @@
+let result1 = 8 + 2.0
+let result2 = 8 - 2.0
+let result3 = 8 * 2.0
+let result4 = 8 / 2.0
+
+print(result1)              // 10.0
+print(type(of: result1))    // Double
+print(result2)              // 6.0
+print(type(of: result2))    // Double
+print(result3)              // 16.0
+print(type(of: result3))    // Double
+print(result4)              // 4.0
+print(type(of: result4))    // Double
