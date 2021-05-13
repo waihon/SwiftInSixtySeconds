e283f7f51bc37a7471c14efa4a193d532eca46be
Waihon Yew <yewwaihon@gmail.com>
2021-05-13 22:04:32 +0800

    Create EmptyDictionaries.swift

---
+++ Dictionaries/EmptyDictionaries.swift
@@ -0,0 +1,9 @@
+var books: [String: String] = [:]
+var leadSingers = [String: String]()
+var olympics: Dictionary<Int, String> = [:]
+var songs = Dictionary<String, String>()
+
+books["Great at Work"] = "Morten T Hansen"
+leadSingers["Coldplay"] = "Chris Martin"
+olympics[2016] = "Rio de Janeiro"
+songs["Someone Like You"] = "Adele"
