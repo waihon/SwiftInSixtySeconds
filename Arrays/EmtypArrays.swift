578576fc854f0c51d2d9f739de0428dd29eae1f0
Waihon Yew <yewwaihon@gmail.com>
2021-05-13 20:21:39 +0800

    Create EmtypArrays.swift

---
+++ Arrays/EmtypArrays.swift
@@ -0,0 +1,9 @@
+var books: [String] = []
+var members = [String]()
+var teams: Array<String> = []
+var songs = Array<String>()
+
+books.append("Great at Work")
+members.append("Simon Le Bon")
+teams.append("Liverpool")
+songs.append("Wild Boys")
